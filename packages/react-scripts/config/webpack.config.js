65f413000b53846f98bdd9baf32b65839ac18846
Davide Fiorello <davide@codeflyer.com>
2019-11-05 16:14:36 +0100

    fixed typo

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -294,7 +294,7 @@ module.exports = function(webpackEnv) {
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
         // Cesium module name
-        // TO USE original cesium package switch the linnes below
+        // TO USE original cesium package switch the lines below
         // cesium: path.resolve(cesiumSource, 'Cesium.js'),
         '@skycatch/cesium': path.resolve(cesiumSource, 'Cesium.js'),
       },
